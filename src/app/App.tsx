aae60610c2def03e10f2e78607a70e1609fa3dfc
dragonmm98 <3d.dragonmm@98gmail.com>
2024-06-01 15:16:45 +0900

    feat: customized integration Material UI

---
+++ src/app/App.tsx
@@ -0,0 +1,26 @@
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import React from 'react';
+import '../css/App.css';
+import { RippleBadge } from './MaterialTheme/styled';
+
+function App() {
+  return (
+  <Container maxWidth="sm">
+    <Stack flexDirection={"column"}>
+      <Box sx={{my:4}}>
+        <Typography variant='h4' component={"h1"} gutterBottom>
+          Create App on Typecript with REDUX
+        </Typography>
+        <RippleBadge badgeContent={4}>
+        <Button color="secondary" variant="contained">
+  Disable elevation
+</Button>
+</RippleBadge>
+        </Box> 
+    </Stack>
+
+  </Container>
+  );
+}
+
+export default App;
